78231592b150900b7795293f1a86257dece974cc
Kypshak09 <amirzhunusov09@gmail.com>
2022-12-22 16:24:09 +0600

    added rightNavigationButton

--- Todoshka/Controllers/InfromationViewController.swift
+++ Todoshka/Controllers/InfromationViewController.swift
@@ -7,23 +7,63 @@
 
 import UIKit
 import SnapKit
+import CoreData
 
 class InfromationViewController: UIViewController {
-
-    let textField: UITextField = {
-        let textField = UITextField()
-        textField.placeholder = "Enter information about your goal"
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+//    let buttonRight: UIBarButtonItem = {
+//        let button = UIBarButtonItem()
+//        button.tintColor = .black
+//        button.setBackgroundImage(UIImage(systemName: "add"), for: .normal, barMetrics: .default)
+//        return button
+//    }()
+    
+//    let buttonRightCorner: UIButton = {
+//        let button = UIButton()
+//        button.tintColor = .black
+//        button.setImage(UIImage(systemName: "add"), for: .normal)
+//        return button
+//    }()
+//
+    let textField: UITextView = {
+        let textField = UITextView()
+        textField.font = .systemFont(ofSize: 20, weight: .light)
         return textField
     }()
+    
+    @objc func rightButton() {
+        print("is working")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(rightButton))
+        
         view.addSubview(textField)
         textField.snp.makeConstraints { make in
-            make.centerY.centerX.equalToSuperview()
-            make.height.width.equalTo(200)
+            make.centerY.equalToSuperview().offset(-200)
+            make.centerX.equalToSuperview().offset(-50)
+            make.width.equalTo(300)
+            make.height.equalTo(400)
+        }
+//        view.addSubview(buttonRight)
+//        buttonRight.snp.makeConstraints { make in
+//            make.centerX.equalToSuperview().offset(300)
+//            make.centerY.equalToSuperview().offset(100)
+//            make.width.height.equalTo(100)
+//        }
+    }
+    
+    
+    
+    func save() {
+        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+            appDelegate.saveContext()
         }
     }
+    
 }
